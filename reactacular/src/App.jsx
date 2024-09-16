9847497760ee65b6401c4015b371bc2ef6a0766c
Jessica Kincaid <4j.kincaid@gmail.com>
2024-09-16 15:23:16 -0500

    return to Project 1 state

--- reactacular/src/App.jsx
+++ reactacular/src/App.jsx
@@ -1,28 +1,37 @@
-import { Cat2 } from './Images.jsx';
-import  Gallery from './Component1.jsx';
-
-// Import Picture2()
-
-// Import AllThePictures()
-
-// import { AllTheCats } from './Images.jsx';
-
-// Import Picture2()
-
-// Import AllThePictures()
-
-// Cat key:
-// 1. hacking keyboard
-// 2. playing under computer
-// 3. looking at itself on computer
-
-// ===================================
-
-export default function App() {
+function Kitten() {
 	return (
-	<>
-		<Gallery />
-		<AllTheCats />
-	</>
-		);
-}
+	<img
+	src="https://upload.wikimedia.org/wikipedia/commons/a/aa/Cat_keyboard.jpg"
+	alt="Kitten hacking on a keyboard"
+	/>
+	);
+  }
+	function Cat2() {
+	  return (
+	  <img
+	  src="https://upload.wikimedia.org/wikipedia/commons/2/22/Apple_computer_cat.jpg"
+	  alt="Cat playing under a computer"
+	  />
+	  );
+	}
+  
+	function App3() {
+	  return (
+	  <img
+	  src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Cat_stares_at_itself_on_computer_monitor.jpg"
+	  alt="Cat looking at itself on computer"
+	  />
+	  );
+	}
+	export default function Gallery() {
+	return (
+	<section>
+	<h1>The Internet. Built for Cats.</h1>
+	<p> Next: Let us vote!</p>
+	<Kitten />
+	<Cat2 />
+	<App3 />
+	</section>
+	);
+	}
+  

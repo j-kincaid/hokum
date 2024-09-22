111184c61b34716fdd4e5ac2c4db588fd26c3148
Jessica Kincaid <4j.kincaid@gmail.com>
2024-09-21 19:16:34 -0500

    add more cats, expressions

--- cat-tricks/src/App.jsx
+++ cat-tricks/src/App.jsx
@@ -3,8 +3,6 @@
 
 
 
-
-
 // Put all of these into separate functions, and call all the functions in the export default function of your component. If you'd like to break these up into separate files, that is totally fine.
 
 function Tricks() {
@@ -18,6 +16,7 @@ function Tricks() {
 }
 
 // Using curly braces for attributes
+// I want to be able to see all of the images so I reduced their size. It didn't work to assign maxWidth inline so I put it in index.css
 function BracesInAttributes() {
   const image = 'https://upload.wikimedia.org/wikipedia/commons/9/98/Patterned_Tabby_Cat_Standing.jpg';
   const description = 'This is a cat standing and reaching for a treat';
@@ -32,47 +31,72 @@ function BracesInAttributes() {
 
 // Using curly braces inside an HTML tag
 function BracesInHTML() {
+  const title1 = 'Cat Tricks'
   const trick1 = 'handshake'
-  const trick2 = 'stand'
+  const trick2 = 'stand on its hind legs and beg'
   return (
-    <h3>Your cat can learn to do a {trick1} and it can learn to {trick2} on its hind legs and beg for treats!</h3>
+    <>
+    <h1>{title1}.</h1>
+    <h3>Your cat can learn to do a {trick1} and it can learn to {trick2} for treats!</h3>
+    </>
   );
 }
 
 // Using curly braces for in-line CSS
 function InlineStyles() {
-	return (
+  return (
 		<div style={
-		{
-			backgroundColor: 'orange', 
-			color: 'black'
-		}
-	}>
-  <p>Your cat will enjoy the challenge!</p>
-
-    </div>
-  );
+      {
+      backgroundColor: 'darkgoldenrod', 
+			color: '#fff',
+      fontSize: '1.5rem',
+      margin: '5%',
+      padding: '0.25rem'
+      }
+    }
+    >
+<h5>You will both enjoy the challenge!</h5>
+  </div>
+  )
 }
-
 
 // Using curly braces for an object with both a name and a theme
 const catPerformer = {
-  name: 'Sweetums',
+  name: 'Marvin',
   theme: {
-    backgroundColor: 'goldenrod',
-    color: 'black'
+    backgroundColor: 'white',
+    color: 'black',
   }
 };
 
-
+function CombinedExpressions() {
+  const human="https://upload.wikimedia.org/wikipedia/commons/c/cc/Handshaking_with_my_master_%284303758332%29.jpg"
+  const description = "Cat shaking hands with human"
+  return (
+    <div style={catPerformer.theme}>
+      <h3>{catPerformer.name} is a talented cat</h3>
+    <img 
+    className="cat"
+    src={human}
+    alt={description}
+    />
+        <img
+        className="pyramid"
+        src="https://upload.wikimedia.org/wikipedia/commons/6/65/Rat%2C_cat_%26_dog_pyramid_at_Fisherman%27s_Wharf.JPG" 
+        alt="pyramid of animals: rat, cat and dog" 
+        />
+    </div>
+  )
+}
 
 
 export default function App() {
   return (
     <>
+      <BracesInHTML />
       <Tricks />
       <BracesInAttributes />
-      <BracesInHTML />
+      <CombinedExpressions />
       <InlineStyles />
       <catPerformer />
     </>

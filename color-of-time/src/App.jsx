261387d766c07c9e9517ff3c4c3ca10b6ce8b8d2
Jessica Kincaid <4j.kincaid@gmail.com>
2024-10-04 17:44:02 -0500

    made progress in reasoning, not in facts

--- color-of-time/src/App.jsx
+++ color-of-time/src/App.jsx
@@ -5,15 +5,43 @@
 // If the time is between 6 PM (18:00) and 12 AM (24:00), show "Good Evening." in purple
 // If the time is between 12 AM (24:00) and 6 AM, show "Good Night." in dark blue
 
-let currentHour = new Date.getHours();
-let color = '';
-if (currentHour > 6 && currentHour < 12) {
-  return <h1 style={{color:'orange'}}>Good Morning.</h1>
-  } if (currentHour > 12 && currentHour < 18) {
-    return <h1 style={{color:'blue'}}>Good Afternoon.</h1>
-    } if (currentHour > 18 && currentHour < 24) {
-      return <h1 style={{color:'purple'}}>Good Evening.</h1> 
-     } if (currentHour > 0 && currentHour < 6) 
-      return <h1 style={{color:'darkblue'}}>Good Night.</h1> 
-        
-      
+// let currentHour = new Date.getHours();
+// let color = '';
+// if (currentHour > 6 && currentHour < 12) {
+//   return <h1 style={{color:'orange'}}>Good Morning.</h1>
+//   } if (currentHour > 12 && currentHour < 18) {
+//     return <h1 style={{color:'blue'}}>Good Afternoon.</h1>
+//     } if (currentHour > 18 && currentHour < 24) {
+//       return <h1 style={{color:'purple'}}>Good Evening.</h1> 
+//      } if (currentHour > 0 && currentHour < 6) 
+//       return <h1 style={{color:'darkblue'}}>Good Night.</h1> 
+        const currentHour = new Date.getHours();
+        // const greetings = ['Morning', 'Afternoon', 'Evening', 'Night'];
+        // const colors = ['orange', 'blue', 'purple', 'darkblue'];
+        // for (i in colors)
+      function CheckColorGreeting({currentHour, color}) {
+        return <h1 className="hour">Good + ' '{greeting}</h1>;
+      }
+
+// let color = "";
+// let greeting = "";
+
+      function TernaryCheckColorGreeting({ color, currentHour }) {
+          return (
+            <h1 className="hour">
+            {	{currentHour  > 6  : currentHour ? < 12 } }
+            {	{currentHour ? ( {color + } ) : (color)} }
+              {color} {currentHour}
+
+            </h1>
+          );
+      }
+
+
+
+      export default function ColorGreeting() {
+        return (
+          <CheckColorGreeting
+
+        )
+      }

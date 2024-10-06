63fd43073f7231e2d0fab135024789b224ac4f3b
Jessica Kincaid <4j.kincaid@gmail.com>
2024-10-06 17:55:17 -0500

    added more exports but [vite] warning: This assignment will throw because "greetingStyle" is a constant

--- color-of-time/src/App.jsx
+++ color-of-time/src/App.jsx
@@ -1,51 +1,67 @@
-const greeting = 18;
-const currentTime = "";
-function ColorTimeOriginal({ greeting, currentTime}) {
-  return <h1 className="day">{currentTime}</h1>
+const currentHour = 18;
+const greeting = "";
+const greetingStyle = "";
+function ColorTimeOriginal({ currentHour, greeting}) {
+  return <h1 className="day">{currentHour}</h1>
+}
+function ColorTimeWithChecks({ currentHour, greeting}) {
+  if (greeting) {
+    return <li className="day">{currentHour} + ' ' + {greeting}</li>
+  }
+  return <li className="day">{currentHour}</li>
+}
+
+function ToIgnore({ currentHour, greeting}) {  
+  if (!currentHour) {
+    return null;
+  }
+  return <li className="day">{greeting}</li>
 }
 
 
-
-function ColorcurrentTimeTernary({greeting, currentTime}) {
+function ColorHourTernary({ currentHour, greeting }) {
   return (
     <h1 className="day">
-    {/* If the hour is between 6 AM and 12 PM, set the currentTime to "Good Morning". */}
 
-     {currentTime ? (greeting >= 6 && greeting <= 12) : (currentTime.style.color = "orange") } 
-     {currentTime ? (greeting > 12 && greeting <= 18) : (currentTime.style.color = "blue") } 
-     {currentTime ? (greeting > 18 && greeting <= 24) : (currentTime.style.color = "purple") }
-     {currentTime ? (greeting > 24 && greeting <= 0) : (currentTime.style.color = "darkblue") }
+     {greeting ? (currentHour >= 6 && currentHour <= 12) : (greetingStyle = "orange") } 
+     {greeting ? (currentHour > 12 && currentHour <= 18) : (greetingStyle = "blue") } 
+     {greeting ? (currentHour > 18 && currentHour <= 24) : (greetingStyle = "purple") }
+     {greeting ? (currentHour > 24 && currentHour <= 0) : (greetingStyle = "darkblue") }
+  
     </h1>
-  )
+  );
 }
 
 
-      export default function MycurrentTime() {
+      export default function MycurrentHour() {
         return (
           <section>
             <ColorTimeOriginal
-            currentTime=""
+            currentHour
             greeting="Good Morning"
             />
-
-            <ColorcurrentTimeTernary
-              greeting={7}
-              currentTime = "Good Morning"
+            <ColorTimeWithChecks
+            currentHour
+            greeting="Good Afternoon"
+            />
+            <ColorHourTernary
+              currentHour
+              greeting = "Good Morning"
               />
 
-            <ColorcurrentTimeTernary
-              greeting={14}
-              currentTime = "Good Afternoon"
+            <ColorHourTernary
+              currentHour={14}
+              greeting = "Good Afternoon"
               />
 
-            <ColorcurrentTimeTernary
-              greeting={21}
-              currentTime = "Good Afternoon"
+            <ColorHourTernary
+              currentHour={21}
+              greeting = "Good Afternoon"
               />
 
-            <ColorcurrentTimeTernary
-              greeting={2}
-              currentTime = "Good Night"
+            <ColorHourTernary
+              currentHour={2}
+              greeting = "Good Night"
               />
             </section>
         )

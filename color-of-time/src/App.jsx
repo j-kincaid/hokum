8b355d3f41b2cc34a4ec6dbfb9a6fa4ee60daa0a
Jessica Kincaid <4j.kincaid@gmail.com>
2024-10-08 17:12:06 -0500

    Working since 2pm and now there are no errors in my console and but still nothing printed to the screen

--- color-of-time/src/App.jsx
+++ color-of-time/src/App.jsx
@@ -1,72 +1,42 @@
-const d = new Date();
-const currentHour = d.getHours() + 1; 
-// const currentHour = 18;
+const currentHour = 18;
 const greeting = "";
-let greetingStyle = "greeting.style.color";
-
-
-// if currentHour is within these values ? use these colors and verbiage:
-
-
-// {greeting ? (currentHour > 24 && currentHour <= 6) : (greetingStyle = "darkblue") }
-// {greeting ? (currentHour > 12 && currentHour <= 18) : (greetingStyle = "blue") } 
-// {greeting ? (currentHour > 18 && currentHour <= 24) : (greetingStyle = "purple") }
-// (currentHour >= 6 ? greetingStyle : "orange") } 
-
-// OR 
-
-// currentHour >= 18 ? : 
-// currentHour >= 12 ? :
-// currentHour >= 6 ? :
-// else
-
-
 function ColorTimeTernary({ greeting, currentHour }) {
-  return (
-    <h1 className="day">
-    {greeting}
-    {currentHour > 18}
-    {currentHour > 12}
-    {currentHour > 6}
-    {currentHour}
 
-     {/* {greeting ? (currentHour >= 6 && currentHour <= 12) : (greeting) }  */}
-     {/* {greeting ? (currentHour > 12 && currentHour <= 18) : (greeting) }  */}
-     {/* {greeting ? (currentHour > 18 && currentHour <= 24) : (greeting) }  */}
-     {greeting}
-    </h1>
-  );
-}
+  
+  
 
-      export default function MycurrentHour() {
+    {greeting ? (currentHour >= 6 && currentHour <= 12) : (greeting = "Good Morning") } 
+    {greeting ? (currentHour > 12 && currentHour <= 18) : (greeting = "Good Afternoon") }
+    {greeting ? (currentHour > 18 && currentHour <= 24) : (greeting = "Good Evening") }    
+    {greeting ? (currentHour >= 1 && currentHour <= 6) : (greeting = "Good Night") }
+
+    const greetingStyle = "greeting.style.color";
+    let color = "";
+
+    {color ? (currentHour >= 6 && currentHour < 12) : (color = '=orange') } 
+    {color ? (currentHour > 12 && currentHour < 18) : (color = '=blue') } 
+    {greetingStyle ? (currentHour > 18 && currentHour < 24) : (color = '=purple') } 
+    {color ? (currentHour >= 1 && currentHour < 6) : (color = '=darkblue') } 
+
+
+    return (
+      <h1 style={greetingStyle}>
+      {greeting}
+      </h1>
+    );
+};
+
+      export default function MyApp() {
         return (
-          <section>
-          <h1>Color of Time</h1>
-          <ul>
-            <Day 
-              currentHour={7}
-              greeting = "Good Morning"
-              greetingStyle = 'orange'
-              />
-          </ul>
+        <section>
+            <ColorTimeTernary />
 
-            <ColorHourTernary
-              currentHour={14}
-              greeting = "Good Afternoon"
-              greetingStyle = 'blue'
-              />
+            <ColorTimeTernary />
 
-            <ColorHourTernary
-              currentHour={21}
-              greeting = "Good Afternoon" 
-              greetingStyle = 'purple'
-              />
+            <ColorTimeTernary />
 
-            <ColorHourTernary
-              currentHour={2}
-              greeting = "Good Night"
-              greetingStyle = 'darkblue'
-              />
-            </section>
-        )
-      }
+            <ColorTimeTernary />
+
+        </section>
+  )
+}

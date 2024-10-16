363ed3f155b4480e180a5fb3ca8b27a0c9e5acdb
Jessica Kincaid <4j.kincaid@gmail.com>
2024-10-15 21:25:39 -0500

    start tutorial docs

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -1,77 +1,22 @@
-function BasicList() {
-	return (
-		<ul>
-			<li>Spongebob: Sponge</li>
-			<li>Patrick: Starfish</li>
-			<li>Squidward: Squid</li>
-			<li>Mrs. Squarepants: Sponge</li>
-			<li>Mrs. Star: Starfish</li>
-		</ul>
-	);
-	
-}
-
-function BasicConst() {
-const characters = [
-	'Spongebob: Sponge',
-	'Patrick: Starfish',
-	'Squidward: Squid',
-	'Mrs. Squarepants: Sponge',
-	'Mrs. Star: Starfish',
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
 ];
 
-	const listItems = characters.map(theCharacter =>
-		<li>{theCharacter}</li>);
-		
-		return (
-		<ul>{listItems}</ul>
-		);
-}
+export default function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
 
-function UsingAFilter() {
-const characters = [{
-	id: 0,
-	name: 'Spongebob',
-	species: 'Sponge',
-	},{
-	id: 1,
-	name: 'Patrick',
-	species: 'Starfish',
-	},{
-	id: 2,
-	name: 'Squidward',
-	species: 'Squid',
-	},{
-	id: 3,
-	name: 'Mrs. Squarepants',
-	species: 'Sponge',
-	},{
-	id: 4,
-	name: 'Mrs. Star',
-	species: 'Starfish',
-	}];
-
-const starfish = characters.filter(filteredCharacter =>
-	filteredCharacter.species === 'Starfish'
-	); 
-	
-const listItems = starfish.map(filteredCharacter =>
-	<li key={filteredCharacter.id}>
-		<b>{filteredCharacter.name}</b>
-		{' '} is known to be a {' '}
-		{filteredCharacter.species}
-	</li>);
-	
-return <ul>{listItems}</ul>
-}
-	
-export default function App() {
-	
-	return (
-	<>
-	<BasicConst />
-	<BasicList />
-	<UsingAFilter />
-	</>
-	);
+  return (
+    <ul>{listItems}</ul>
+  );
 }

a129434e44ac615579ebfc6cede27470462faa58
Jessica Kincaid <4j.kincaid@gmail.com>
2024-10-22 21:38:16 -0500

    Complete Project 7

--- events/src/App.jsx
+++ events/src/App.jsx
@@ -1,113 +1,97 @@
-function IamAButton() {
-  return (
-    <button>
-      I am a button
-    </button>
-  );
-}
+ // Button 1: Create a button that triggers an alert when it is clicked.
 
-function ButtonWithHandler() {
-	function handleClick() {
-		alert('Good choice.');
-	}
+    function ButtonOne() {
+      function handleClick() {
+        alert('This is your alert!');
+      }
+      return (
+        <button onClick={handleClick}>
+          Click for Alert
+        </button>
+      );
+    }
 
-	return (
-		<button onClick={handleClick}>
-		Click me
-		</button>
-	);
-}
-function EventHandlerProps({message, children}) {
-  return (
-    <button onClick={() => alert (message)}>
-      {children}
-    </button>
-  );
-}
+    // Button 2: Create a button that triggers a console log when it is clicked.
 
+    function ButtonTwo() {
+      function handleClick() {
+        console.log('Hello from the console!');
+      }
 
+      return (
+        <button onClick={handleClick}>
+        Click to log to console
+        </button>
+      );
+    }
 
-{/*event handlers as props */}
+    // Button 3: Create a button that becomes disabled and changes the text when it is clicked.
 
-function EventHandlersAsPropsButton({ onClick, children}) {
-  return (
-    <button onClick={onClick}>
-      {children}
-    </button>
-  );
-}
+    // function Button3() {
+    //   JS disable this button;
+    //   JS change textContent;
 
-function PlayTheMovie({ movie }) {
-  function handlePlayClick() {
-    alert(`${movie} is my favorite!`);
-  }
+    function ButtonThree() {
+      function handleClick() {
+        button.b3.disabled="True";
+        button.b3.textContent="Disabled button";
+      }
+      
 
-  return (
-    <EventHandlersAsPropsButton onClick={handlePlayClick}>
-      Play "{movie}"
-    </EventHandlersAsPropsButton>
-  );
-}
-
-function UploadYourImage() {
-  return (
-    <EventHandlersAsPropsButton onClick={() => alert('Thank you, it is beautiful.')}>
-      Give Me Your Headshot
-    </EventHandlersAsPropsButton>
-  );
-}
-
-{ /* end of event handlers as props */}
+      // function ButtonThree({message, Enabled}) {
+      //   function handleClick() {
+      //     <ButtonThree>
+      //       Disable
+      //     </ButtonThree>
+      //   }
 
 
-{/* Custom handler names */}
+      function ToDisableButtonThree ({ message, enabled }) {
+        return (
+          <button className="b3">
 
-function CustomHandlerNameButton({ onClick, children }) {
-  return (
-    <button onClick={onClick}>
-      {children}
-    </button>
-  );
-}
+            {enabled ? message = 'Click to disable' :  message}
+          </button>
+        );
+      }
 
-function TheGoods({ onPlayIceAge, onUploadHeadshot }) {
-  return (
-    <div>
-      <CustomHandlerNameButton onClick={onPlayIceAge}>
-        Watch Ice Age!
-      </CustomHandlerNameButton>
-      <CustomHandlerNameButton onClick={onUploadHeadshot}>
-        Let me make fun of your photo
-      </CustomHandlerNameButton>
-    </div>
-  );
-}
+      }
+    //   return (
+    //     <button className="b3" onClick={handleClick}>
+    //     Disable me
+    //     </button>
+    //   );
+    // }
 
 
-{/* End of custom handler names */}
+    // Button 4: Create a button that triggers an alert when it is DOUBLE clicked.
 
-export default function App() {
 
-return (
-<>
-<IamAButton />
-  <ButtonWithHandler />
-      <div>
-        <EventHandlerProps message="You pressed the button!">
-          Press the button?
-        </EventHandlerProps>
-        <EventHandlerProps message="Good job!">
-          Or not?
-        </EventHandlerProps>
-      </div>
-    <div>
-        <PlayTheMovie movie="Ice Age" />
-        <UploadYourImage />
-      </div>
-    <TheGoods
-        onPlayIceAge={() => alert('A cinematic masterpiece')}
-        onUploadHeadshot={() => alert('This is just ok.')}
-      />
-  </>
-  )
-}
+    function ButtonFour() {
+      function handleDoubleClick() {
+        alert('You DOUBLE clicked!');
+      }
+
+      return (
+        <button onDoubleClick={handleDoubleClick}>
+          Double-click me
+        </button>
+      );
+    }
+
+
+
+    export default function App() {
+
+      return (
+      
+        <>
+        <h1>I am stuck on Button 3 but the others are ok</h1>
+        <ButtonOne />
+        <ButtonTwo />
+        <ButtonThree />
+        <ButtonFour />
+        </>
+       
+      )
+    }
